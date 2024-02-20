24b44aca2f7a14292bd6fe826762e8791ad7ff62
oscarfiscal <oscarfiscalcp@gmail.com>
2024-02-20 01:05:54 -0500

    feat: Add delete button with confirmation modal for products
    In this commit, a delete button has been added to each product item in the user interface. Clicking on this button triggers a modal window to confirm the deletion of the respective product. Additionally, functionality for handling the deletion process has been implemented in the controller.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -69,8 +69,9 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect()->route('products.index');
     }
 }
